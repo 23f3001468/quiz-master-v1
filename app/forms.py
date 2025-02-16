32b274d9a9a7290272970dee12fbeee414a54b45
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-02-16 21:27:36 +0530

    Added the Flask forms for question creation

--- app/forms.py
+++ app/forms.py
@@ -17,3 +17,27 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField("Sign Up")
 
+class SubjectForm(FlaskForm):
+    name = StringField("Subject Name", validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    submit = SubmitField("Add Subject")
+
+class ChapterForm(FlaskForm):
+    name = StringField("Chapter Name", validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    submit = SubmitField("Add Chapter")
+
+class QuizForm(FlaskForm):
+    date_of_quiz = StringField("Date of Quiz", validators=[DataRequired()])
+    duration = StringField("Duration", validators=[DataRequired()])
+    remarks = TextAreaField("Remarks")
+    submit = SubmitField("Add Quiz")
+
+class QuestionForm(FlaskForm):
+    question = TextAreaField("Question", validators=[DataRequired()])
+    option1 = StringField("Option 1", validators=[DataRequired()])
+    option2 = StringField("Option 2", validators=[DataRequired()])
+    option3 = StringField("Option 3", validators=[DataRequired()])
+    option4 = StringField("Option 4", validators=[DataRequired()])
+    correct_option = SelectField("Correct Option", choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4')], validators=[DataRequired()])
+    submit = SubmitField("Add Question")
