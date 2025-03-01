33b73f4cc40895b621937bb6b29c7b8c3d130d64
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-03-01 20:13:45 +0530

    Added the feature to edit and delete the subject

--- app/routes.py
+++ app/routes.py
@@ -57,6 +57,7 @@ def dashboard():
     subjects = Subject.query.all()
     return render_template("dashboard.html",subjects=subjects, user_role=current_user.role)
 
+#subject routes
 @main.route('/add_subject', methods=['GET', 'POST'])
 @login_required
 def add_subject():
@@ -66,9 +67,35 @@ def add_subject():
         db.session.add(subject)
         db.session.commit()
         flash(f'Subject {form.name.data} added successfully!', 'success')
-        return redirect(url_for('main.add_subject'))
+        return redirect(url_for('main.dashboard'))
     return render_template("add_subject.html", form=form)
 
+@main.route('/delete_subject/<int:subject_id>', methods=['GET', 'POST'])
+@login_required
+def delete_subject(subject_id):
+    subject = Subject.query.get(subject_id)
+    db.session.delete(subject)
+    db.session.commit()
+    flash(f'Subject {subject.name} deleted successfully!', 'success')
+    return redirect(url_for('main.dashboard'))
+
+@main.route('/edit_subject/<int:subject_id>', methods=['GET', 'POST'])
+@login_required
+def edit_subject(subject_id):
+    subject = Subject.query.get(subject_id)
+    form = SubjectForm()
+    if form.validate_on_submit():
+        subject.name = form.name.data
+        subject.description = form.description.data
+        db.session.commit()
+        flash(f'Subject {form.name.data} edited successfully!', 'success')
+        return redirect(url_for('main.dashboard'))
+    elif request.method == 'GET':
+        form.name.data = subject.name
+        form.description.data = subject.description
+    return render_template("edit_subject.html", form=form,subject=subject)
+
+#chapter routes
 @main.route('/add_chapter/<int:subject_id>', methods=['GET', 'POST'])
 @login_required
 def add_chapter(subject_id):
@@ -80,6 +107,32 @@ def add_chapter(subject_id):
         flash(f'Chapter {form.name.data} added successfully!', 'success')
         return redirect(url_for('main.add_chapter'))
     return render_template("add_chapter.html", form=form)
+
+@main.route('/delete_chapter/<int:chapter_id>', methods=['GET', 'POST'])
+@login_required
+def delete_chapter(chapter_id):
+    chapter = Chapter.query.get(chapter_id)
+    db.session.delete(chapter)
+    db.session.commit()
+    flash(f'Chapter {chapter.name} deleted successfully!', 'success')
+    return redirect(url_for('main.dashboard'))
+
+@main.route('/edit_chapter/<int:chapter_id>', methods=['GET', 'POST'])
+@login_required
+def edit_chapter(chapter_id):
+    chapter = Chapter.query.get(chapter_id)
+    form = ChapterForm()
+    if form.validate_on_submit():
+        chapter.name = form.name.data
+        chapter.description = form.description.data
+        db.session.commit()
+        flash(f'Chapter {form.name.data} edited successfully!', 'success')
+        return redirect(url_for('main.dashboard'))
+    elif request.method == 'GET':
+        form.name.data = chapter.name
+        form.description.data = chapter.description
+    return render_template("edit_chapter.html", form=form)
+
 
 @main.route('/add_quiz/<int:chapter_id>', methods=['GET', 'POST'])
 @login_required
@@ -108,7 +161,8 @@ def add_question(quiz_id):
 @main.route('/subjects/<int:subject_id>')
 def subject(subject_id):
     chapters = Chapter.query.filter_by(subjectId=subject_id).all()
-    return render_template("subject.html", chapters=chapters,user_role=current_user.role)
+    subject = Subject.query.filter_by(id=subject_id).first()
+    return render_template("subject.html",chapters=chapters,user_role=current_user.role,subject=subject)
 
 @main.route('/logout')
 def logout():
